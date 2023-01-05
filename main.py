2f6d2ede8d44a53d4221f5b989036caa974b3d73
NotchApple1703 <96767159+NotchApple1703@users.noreply.github.com>
2023-01-05 16:31:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+"""Import modules"""
+import discord
+import json
+import requests
+
+"""Define bot"""
+bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+
+@bot.command(description = "Input Url")
+async def url(ctx, url):
+    response = requests.get(f"https://traffic1s.com/get-codes?hostname={url}", headers = {"Origin": f"https://{url}"})
+    result = json.loads(response.text)
+    await ctx.respond(result["html"])
+
+@bot.command(description = "Help")
+async def help(ctx):
+    await ctx.respond(file = discord.File("help.mp4"))
+
+
+if __name__ == "__main__":
+    bot.run("MTA2MDQ1NDkwMTg5OTA2NzM5Mg.GO1pQ6.CzQo2RoQhxs9Ri26rXDuGCoZhd7Evmlj8-ArVI")
